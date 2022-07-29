10592469e80121eca65cea15d3ea7477ab32d05e
lim <sherwinlim8899@gmail.com>
2022-07-29 14:05:59 +0800

    add libraries

---
+++ Form1.Designer.vb
@@ -0,0 +1,202 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.addbtn = New System.Windows.Forms.Button()
+        Me.savebtn = New System.Windows.Forms.Button()
+        Me.clearbtn = New System.Windows.Forms.Button()
+        Me.FullName = New System.Windows.Forms.TextBox()
+        Me.tb1 = New System.Windows.Forms.TextBox()
+        Me.lb1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.getfilebtn = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'addbtn
+        '
+        Me.addbtn.Location = New System.Drawing.Point(332, 92)
+        Me.addbtn.Name = "addbtn"
+        Me.addbtn.Size = New System.Drawing.Size(75, 23)
+        Me.addbtn.TabIndex = 1
+        Me.addbtn.Text = "Add"
+        Me.addbtn.UseVisualStyleBackColor = True
+        '
+        'savebtn
+        '
+        Me.savebtn.Location = New System.Drawing.Point(332, 121)
+        Me.savebtn.Name = "savebtn"
+        Me.savebtn.Size = New System.Drawing.Size(75, 23)
+        Me.savebtn.TabIndex = 2
+        Me.savebtn.Text = "Save"
+        Me.savebtn.UseVisualStyleBackColor = True
+        '
+        'clearbtn
+        '
+        Me.clearbtn.Location = New System.Drawing.Point(332, 150)
+        Me.clearbtn.Name = "clearbtn"
+        Me.clearbtn.Size = New System.Drawing.Size(75, 23)
+        Me.clearbtn.TabIndex = 3
+        Me.clearbtn.Text = "Clear"
+        Me.clearbtn.UseVisualStyleBackColor = True
+        '
+        'FullName
+        '
+        Me.FullName.Location = New System.Drawing.Point(112, 63)
+        Me.FullName.Name = "FullName"
+        Me.FullName.Size = New System.Drawing.Size(214, 23)
+        Me.FullName.TabIndex = 4
+        '
+        'tb1
+        '
+        Me.tb1.Location = New System.Drawing.Point(112, 92)
+        Me.tb1.Name = "tb1"
+        Me.tb1.Size = New System.Drawing.Size(214, 23)
+        Me.tb1.TabIndex = 5
+        '
+        'lb1
+        '
+        Me.lb1.FormattingEnabled = True
+        Me.lb1.ItemHeight = 15
+        Me.lb1.Location = New System.Drawing.Point(112, 120)
+        Me.lb1.Name = "lb1"
+        Me.lb1.Size = New System.Drawing.Size(214, 79)
+        Me.lb1.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(43, 66)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(61, 15)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Full Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(45, 96)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(59, 15)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Type Here"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(77, 120)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(28, 15)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Age"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(56, 135)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(51, 15)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Birthday"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(60, 150)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(45, 15)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Gender"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(55, 165)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(49, 15)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Address"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(8, 180)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(96, 15)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "Contact Number"
+        '
+        'getfilebtn
+        '
+        Me.getfilebtn.Location = New System.Drawing.Point(332, 176)
+        Me.getfilebtn.Name = "getfilebtn"
+        Me.getfilebtn.Size = New System.Drawing.Size(75, 23)
+        Me.getfilebtn.TabIndex = 15
+        Me.getfilebtn.Text = "Get File"
+        Me.getfilebtn.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.getfilebtn)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lb1)
+        Me.Controls.Add(Me.tb1)
+        Me.Controls.Add(Me.FullName)
+        Me.Controls.Add(Me.clearbtn)
+        Me.Controls.Add(Me.savebtn)
+        Me.Controls.Add(Me.addbtn)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents addbtn As Button
+    Friend WithEvents savebtn As Button
+    Friend WithEvents clearbtn As Button
+    Friend WithEvents FullName As TextBox
+    Friend WithEvents tb1 As TextBox
+    Friend WithEvents lb1 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents getfilebtn As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,45 @@
+﻿Public Class Form1
+    Private Sub addbtn_Click(sender As Object, e As EventArgs) Handles addbtn.Click
+        lb1.Items.Add(tb1.Text)
+
+
+    End Sub
+
+    Private Sub savebtn_Click(sender As Object, e As EventArgs) Handles savebtn.Click
+
+        Dim file As System.IO.StreamWriter
+        Dim x As Integer
+        file = New IO.StreamWriter("C:\Users\Lim\OneDrive\Desktop\vbassignment3\" & FullName.Text & ".txt")
+        For x = 0 To lb1.Items.Count - 1
+            file.WriteLine(lb1.Items.Item(x))
+        Next
+
+        file.Close()
+
+    End Sub
+
+    Private Sub clearbtn_Click(sender As Object, e As EventArgs) Handles clearbtn.Click
+
+
+        lb1.Items.Clear()
+
+    End Sub
+
+    Private Sub getfilebtn_Click(sender As Object, e As EventArgs) Handles getfilebtn.Click
+
+        Dim fileReader As System.IO.StreamReader
+        fileReader = My.Computer.FileSystem.OpenTextFileReader("C:\Users\Lim\OneDrive\Desktop\vbassignment3\" & FullName.Text & ".txt")
+        Dim stringReader As String
+
+        While Not fileReader.EndOfStream
+            stringReader = fileReader.ReadLine()
+            stringReader &= vbCrLf & fileReader.ReadLine()
+            stringReader &= vbCrLf & fileReader.ReadLine()
+            stringReader &= vbCrLf & fileReader.ReadLine()
+            stringReader &= vbCrLf & fileReader.ReadLine()
+            MsgBox(stringReader)
+        End While
+        fileReader.Close()
+
+    End Sub
+End Class
